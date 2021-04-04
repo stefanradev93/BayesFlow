0af8cd4c8264ab58910176232168045bb8f14d7f
marvinschmitt <mail@marvin-schmitt.de>
2021-04-04 11:05:52 +0200

    Modify prior batch mode detection

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,7 +23,6 @@ class TestGenerativeModel(unittest.TestCase):
         generative_model = GenerativeModel(ex.model_prior, priors, simulators)
         _, _, _ = generative_model(n_sim=N_SIM, n_obs=N_OBS)
 
-    # @unittest.skip("Meta-Model Optimization only works with simulator output shape (n_sim, n_obs[, *data_dims])")
     def test_meta_generative_model_different_param_shapes(self):
         priors = [ex.model1_params_prior, ex.model2_params_prior, ex.model3_params_prior]
         simulators = [ex.forward_model1, ex.forward_model2, ex.forward_model3]
@@ -37,11 +36,11 @@ class TestSimpleGenerativeModel(unittest.TestCase):
         return generative_model
 
     def test_simulation(self):
-        _n_sim = 10
-        _n_obs = 100
-        generative_model = self.test_init()
-        params, data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
-        # todo validate shapes
+        generative_model = SimpleGenerativeModel(ex.dm_prior, ex.dm_batch_simulator)
+        params, sim_data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
+        self.assertTrue(params.shape[0] == N_SIM)
+        self.assertTrue(sim_data.shape[0] == N_SIM)
+        self.assertTrue(sim_data.shape[1] == N_OBS)
 
 
 class TestMetaGenerativeModel(unittest.TestCase):
@@ -53,21 +52,34 @@ class TestMetaGenerativeModel(unittest.TestCase):
         simulators = [ex.MultivariateT(df) for df in np.arange(1, 101, M)]
         generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
 
-        return generative_model
-
     def test_simulation(self):
-        generative_model = self.test_init()
-        model_idx, params, data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
-        # todo validate shapes
+        M = 10
+        D = 8
+        prior = ex.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
+        priors = [prior] * M
+        simulators = [ex.MultivariateT(df) for df in np.arange(1, 101, M)]
+        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
+
+        model_idx, params, sim_data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
+        self.assertTrue(model_idx.shape[0] == N_SIM)
+        self.assertTrue(params.shape[0] == N_SIM)
+        self.assertTrue(sim_data.shape[0] == N_SIM)
+        self.assertTrue(sim_data.shape[1] == N_OBS)
 
     def test_init_different_param_shapes(self):
         priors = [ex.model1_params_prior, ex.model2_params_prior, ex.model3_params_prior]
         simulators = [ex.forward_model1, ex.forward_model2, ex.forward_model3]
         generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
 
-        return generative_model
-
     def test_simulation_different_param_shapes(self):
-        generative_model = self.test_init_different_param_shapes()
-        model_idx, params, data = generative_model(n_sim=16, n_obs=150)
-        # todo validate shapes
+        _n_sim = 16
+        _n_obs = 150
+        priors = [ex.model1_params_prior, ex.model2_params_prior, ex.model3_params_prior]
+        simulators = [ex.forward_model1, ex.forward_model2, ex.forward_model3]
+        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
+
+        model_idx, params, sim_data = generative_model(n_sim=_n_sim, n_obs=_n_obs)
+        self.assertTrue(model_idx.shape[0] == _n_sim)
+        self.assertTrue(params.shape[0] == _n_sim)
+        self.assertTrue(sim_data.shape[0] == _n_sim)
+        self.assertTrue(sim_data.shape[1] == _n_obs)
