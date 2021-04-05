8ccea2e00df0e07989033a37e4f210ad61c45159
marvinschmitt <mail@marvin-schmitt.de>
2021-04-05 22:47:54 +0200

    Restructure helpers.py

--- bayesflow/helpers.py
+++ bayesflow/helpers.py
@@ -35,6 +35,18 @@ def clip_gradients(gradients, clip_value=5., clip_method='norm'):
     return gradients
 
 
+def merge_left_into_right(left_dict, right_dict):
+    """
+    Function to merge nested dict left_dict into nested dict right_dict
+    """
+    for k, v in left_dict.items():
+        if isinstance(v, dict):
+            right_dict[k] = merge_left_into_right(v, right_dict[k])
+        else:
+            right_dict[k] = v
+    return right_dict
+
+
 def build_meta_dict(user_dict: dict, default_setting: default_settings.MetaDictSetting) -> dict:
     """
     Takes a user-defined dictionary and a default dictionary.
@@ -53,17 +65,6 @@ def build_meta_dict(user_dict: dict, default_setting: default_settings.MetaDictS
     merged dict
 
     """
-
-    def merge_left_into_right(left_dict, right_dict):
-        """
-        Function to merge nested dict left_dict into nested dict right_dict
-        """
-        for k, v in left_dict.items():
-            if isinstance(v, dict):
-                right_dict[k] = merge_left_into_right(v, right_dict[k])
-            else:
-                right_dict[k] = v
-        return right_dict
 
     default_dict = default_setting.meta_dict
     mandatory_fields = default_setting.mandatory_fields
