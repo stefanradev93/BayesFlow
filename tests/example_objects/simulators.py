c7df6517b6ad6d5aee9d46512817e6320f201e3b
marvinschmitt <mail@marvin-schmitt.de>
2021-04-05 22:48:35 +0200

    restructure example objects for tests into separate folder

--- tests/example_objects/simulators.py
+++ tests/example_objects/simulators.py
@@ -3,28 +3,6 @@ from numba import njit
 from scipy import stats
 
 
-def dm_prior(batch_size):
-    """
-    Samples from the prior 'batch_size' times.
-    ----------
-
-    Arguments:
-    batch_size : int -- the number of samples to draw from the prior
-    ----------
-
-    Output:
-    theta : np.ndarray of shape (batch_size, theta_dim) -- the samples batch of parameters
-    """
-
-    # Prior ranges for the simulator
-    # v_c ~ U(-7.0, 7.0)
-    # a_c ~ U(0.1, 4.0)
-    # t0 ~ U(0.1, 3.0)
-    p_samples = np.random.uniform(low=(0.1, 0.1, 0.1, 0.1, 0.1),
-                                  high=(7.0, 7.0, 4.0, 4.0, 3.0), size=(batch_size, 5))
-    return p_samples.astype(np.float32)
-
-
 @njit
 def diffusion_trial(v, a, ndt, zr, dt, max_steps):
     """Simulates a trial from the diffusion model."""
@@ -92,46 +70,6 @@ def dm_batch_simulator(prior_samples, n_obs, dt=0.005, s=1.0, max_iter=1e4):
     return sim_data
 
 
-def model_prior(batch_size, n_models=3, p_vals=None):
-    """
-    Samples from the models' prior batch size times and converts to one-hot.
-    Assumes equal model priors.
-    ----------
-
-    Arguments:
-    batch_size : int  -- the number of samples to draw from the prior
-    ----------
-
-    Returns:
-    m_true : np.ndarray of shape (batch_size, theta_dim) -- the samples batch of parameters
-    """
-
-    # Equal priors, if nothign specified
-    if p_vals is None:
-        p_vals = [1 / n_models] * n_models
-    m_idx = np.random.choice(n_models, size=batch_size, p=p_vals).astype(np.int32)
-    return m_idx
-
-
-class TPrior:
-
-    def __init__(self, theta_dim, mu_scale, scale_scale):
-        self.theta_dim = theta_dim
-        self.prior_mu = stats.multivariate_normal(np.zeros(self.theta_dim), mu_scale * np.eye(self.theta_dim))
-        self.prior_scale = stats.uniform(0, scale_scale)
-
-    def __call__(self, batch_size):
-        """
-        Returns a sample from the prior.
-        """
-
-        mu_samples = self.prior_mu.rvs(batch_size)
-        if batch_size == 1:
-            mu_samples = mu_samples[np.newaxis]
-        scale_samples = self.prior_scale.rvs((batch_size, self.theta_dim))
-        return np.c_[mu_samples, scale_samples].astype(np.float32)
-
-
 class MultivariateT:
     def __init__(self, df=10, seed=42):
         self.df = df
@@ -164,70 +102,6 @@ class MultivariateT:
         """
 
         return self.generate_multiple_datasets(p_samples, n_obs)
-
-
-def model1_params_prior(**args):
-    """
-    Samples from the prior of the HH-2pars theta = (gbar_Na,gbar_K)
-    ----------
-
-    Arguments:
-    ----------
-
-    Output:
-    theta : np.ndarray of shape (1, theta_dim) -- the samples of parameters
-            or a dict with param key-values
-    """
-
-    theta = [
-        np.random.uniform(low=1.5, high=30),
-        np.random.uniform(low=0.3, high=15)
-    ]
-    return np.array(theta)
-
-
-def model2_params_prior(**args):
-    """
-     Samples from the prior of the HH-3pars theta = (gbar_Na,gbar_K,gbar_M)
-    ----------
-
-    Arguments:
-    ----------
-
-    Output:
-    theta : np.ndarray of shape (1, theta_dim) -- the samples of parameters
-            or a dict with param key-values
-    """
-
-    theta = [
-        np.random.uniform(low=1.5, high=30),
-        np.random.uniform(low=0.3, high=15),
-        np.random.uniform(low=0.005, high=0.3)
-    ]
-    return np.array(theta)
-
-
-def model3_params_prior(**args):
-    """
-    Samples from the prior of the HH-4pars theta = (gbar_l,gbar_Na,gbar_K,gbar_M)
-    ----------
-
-    Arguments:
-    ----------
-
-    Output:
-    theta : np.ndarray of shape (1, theta_dim) -- the samples of parameters
-            or a dict with param key-values
-    """
-
-    theta = [
-        np.random.uniform(low=0.01, high=0.18),
-        np.random.uniform(low=1.5, high=30),
-        np.random.uniform(low=0.1, high=15),
-        np.random.uniform(low=0.005, high=0.3)
-    ]
-    return np.array(theta)
-
 
 @njit
 def forward_model1(params, n_obs, V0=-70, I_input=3, dt=0.2):
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-import tests.example_objects_for_tests as ex
+import tests.example_objects as ex
 from bayesflow.models import GenerativeModel, SimpleGenerativeModel, MetaGenerativeModel
 
 N_SIM = 16
@@ -11,32 +11,32 @@ N_OBS = 20
 
 class TestGenerativeModel(unittest.TestCase):
     def test_simple_generative_model(self):
-        generative_model = GenerativeModel(ex.dm_prior, ex.dm_batch_simulator)
-        _, _ = generative_model(n_sim=N_SIM, n_obs=N_OBS)
+        generative_model = GenerativeModel(ex.priors.dm_prior, ex.simulators.dm_batch_simulator)
+        _params, _sim_data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
 
     def test_meta_generative_model(self):
         M = 10
         D = 100
-        prior = ex.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
+        prior = ex.priors.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
         priors = [prior] * M
-        simulators = [ex.MultivariateT(df) for df in np.arange(1, 101, M)]
-        generative_model = GenerativeModel(ex.model_prior, priors, simulators)
-        _, _, _ = generative_model(n_sim=N_SIM, n_obs=N_OBS)
+        simulators = [ex.simulators.MultivariateT(df) for df in np.arange(1, 101, M)]
+        generative_model = GenerativeModel(ex.priors.model_prior, priors, simulators)
+        _model_indices, _params, _sim_data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
 
     def test_meta_generative_model_different_param_shapes(self):
-        priors = [ex.model1_params_prior, ex.model2_params_prior, ex.model3_params_prior]
-        simulators = [ex.forward_model1, ex.forward_model2, ex.forward_model3]
-        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
-        _, _, _ = generative_model(n_sim=16, n_obs=150)
+        priors = [ex.priors.model1_params_prior, ex.priors.model2_params_prior, ex.priors.model3_params_prior]
+        simulators = [ex.simulators.forward_model1, ex.simulators.forward_model2, ex.simulators.forward_model3]
+        generative_model = MetaGenerativeModel(ex.priors.model_prior, priors, simulators)
+        _model_indices, _params, _sim_data = generative_model(n_sim=16, n_obs=150)
 
 
 class TestSimpleGenerativeModel(unittest.TestCase):
     def test_init(self):
-        generative_model = SimpleGenerativeModel(ex.dm_prior, ex.dm_batch_simulator)
+        generative_model = SimpleGenerativeModel(ex.priors.dm_prior, ex.simulators.dm_batch_simulator)
         return generative_model
 
     def test_simulation(self):
-        generative_model = SimpleGenerativeModel(ex.dm_prior, ex.dm_batch_simulator)
+        generative_model = SimpleGenerativeModel(ex.priors.dm_prior, ex.simulators.dm_batch_simulator)
         params, sim_data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
         self.assertTrue(params.shape[0] == N_SIM)
         self.assertTrue(sim_data.shape[0] == N_SIM)
@@ -47,18 +47,18 @@ class TestMetaGenerativeModel(unittest.TestCase):
     def test_init(self):
         M = 10
         D = 8
-        prior = ex.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
+        prior = ex.priors.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
         priors = [prior] * M
-        simulators = [ex.MultivariateT(df) for df in np.arange(1, 101, M)]
-        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
+        simulators = [ex.simulators.MultivariateT(df) for df in np.arange(1, 101, M)]
+        _generative_model = MetaGenerativeModel(ex.priors.model_prior, priors, simulators)
 
     def test_simulation(self):
         M = 10
         D = 8
-        prior = ex.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
+        prior = ex.priors.TPrior(D // 2, mu_scale=1.0, scale_scale=5.0)
         priors = [prior] * M
-        simulators = [ex.MultivariateT(df) for df in np.arange(1, 101, M)]
-        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
+        simulators = [ex.simulators.MultivariateT(df) for df in np.arange(1, 101, M)]
+        generative_model = MetaGenerativeModel(ex.priors.model_prior, priors, simulators)
 
         model_idx, params, sim_data = generative_model(n_sim=N_SIM, n_obs=N_OBS)
         self.assertTrue(model_idx.shape[0] == N_SIM)
@@ -67,19 +67,19 @@ class TestMetaGenerativeModel(unittest.TestCase):
         self.assertTrue(sim_data.shape[1] == N_OBS)
 
     def test_init_different_param_shapes(self):
-        priors = [ex.model1_params_prior, ex.model2_params_prior, ex.model3_params_prior]
-        simulators = [ex.forward_model1, ex.forward_model2, ex.forward_model3]
-        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
+        priors = [ex.priors.model1_params_prior, ex.priors.model2_params_prior, ex.priors.model3_params_prior]
+        simulators = [ex.simulators.forward_model1, ex.simulators.forward_model2, ex.simulators.forward_model3]
+        _generative_model = MetaGenerativeModel(ex.priors.model_prior, priors, simulators)
 
     def test_simulation_different_param_shapes(self):
         _n_sim = 16
         _n_obs = 150
-        priors = [ex.model1_params_prior, ex.model2_params_prior, ex.model3_params_prior]
-        simulators = [ex.forward_model1, ex.forward_model2, ex.forward_model3]
-        generative_model = MetaGenerativeModel(ex.model_prior, priors, simulators)
+        priors = [ex.priors.model1_params_prior, ex.priors.model2_params_prior, ex.priors.model3_params_prior]
+        simulators = [ex.simulators.forward_model1, ex.simulators.forward_model2, ex.simulators.forward_model3]
+        generative_model = MetaGenerativeModel(ex.priors.model_prior, priors, simulators)
 
-        model_idx, params, sim_data = generative_model(n_sim=_n_sim, n_obs=_n_obs)
-        self.assertTrue(model_idx.shape[0] == _n_sim)
+        model_indices, params, sim_data = generative_model(n_sim=_n_sim, n_obs=_n_obs)
+        self.assertTrue(model_indices.shape[0] == _n_sim)
         self.assertTrue(params.shape[0] == _n_sim)
         self.assertTrue(sim_data.shape[0] == _n_sim)
         self.assertTrue(sim_data.shape[1] == _n_obs)
