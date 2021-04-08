e654ecacc17d3b3cb9e8b47ac26af83c053e4051
marvinschmitt <mail@marvin-schmitt.de>
2021-04-08 16:17:56 +0200

    add basic test for InvertibleNetwork

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 import tests.example_objects as ex
 from bayesflow.models import GenerativeModel
-from bayesflow.networks import InvariantNetwork
+from bayesflow.networks import InvariantNetwork, InvertibleNetwork
 
 
 class TestInvariantNetwork(unittest.TestCase):
@@ -15,7 +15,7 @@ class TestInvariantNetwork(unittest.TestCase):
         cls.generative_model = GenerativeModel(ex.priors.dm_prior, ex.simulators.dm_batch_simulator)
         cls.network = InvariantNetwork()
         params, sim_data = cls.generative_model(64, 128)
-        cls.network(sim_data)
+        cls.network(sim_data)  # one prediction to initialize layers
         cls.optimizer = tf.keras.optimizers.Adam()
 
     def test_invariant_network_step(self):
@@ -31,6 +31,34 @@ class TestInvariantNetwork(unittest.TestCase):
 
         trainable_variables_after = copy.deepcopy(self.network.trainable_variables)
 
-        # assert that some weights are updated in each layer
+        # assert that any weights are updated in each layer
+        for before, after in zip(trainable_variables_before, trainable_variables_after):
+            self.assertTrue(np.any(before != after))
+
+
+class TestInvertibleNetwork(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.generative_model = GenerativeModel(ex.priors.dm_prior, ex.simulators.dm_batch_simulator)
+        cls.invariant_network = InvariantNetwork()
+        cls.network = InvertibleNetwork({'n_params': 5})
+        cls.optimizer = tf.keras.optimizers.Adam()
+
+    def test_invertible_network_step(self):
+        params, sim_data = self.generative_model(64, 128)
+
+        sim_data = self.invariant_network(sim_data)
+        _, _ = self.network(params, sim_data)  # initialize layers
+        trainable_variables_before = copy.deepcopy(self.network.trainable_variables)
+        with tf.GradientTape() as tape:
+            z, log_det_J = self.network(params, sim_data)
+            loss = tf.reduce_mean(0.5 * tf.square(tf.norm(z, axis=-1)) - log_det_J)  # kl latent space loss
+
+        gradients = tape.gradient(loss, self.network.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.network.trainable_variables))
+
+        trainable_variables_after = copy.deepcopy(self.network.trainable_variables)
+
+        # assert that any weights are updated in each layer
         for before, after in zip(trainable_variables_before, trainable_variables_after):
             self.assertTrue(np.any(before != after))
