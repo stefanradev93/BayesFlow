64fa58630a23f018a1aa957d55a36d0d88140c61
Marvin Schmitt <mail@marvinschmitt.com>
2022-08-10 14:58:41 +0200

    fix default parameter names

--- bayesflow/benchmarks/gaussian_linear.py
+++ bayesflow/benchmarks/gaussian_linear.py
@@ -20,7 +20,7 @@ bayesflow_benchmark_info = {
     'generative_model_info':
         {
             'simulator_is_batched': True,
-            'parameter_names': [],
+            'parameter_names': None,
         },
     'configurator_info': {}
 }
