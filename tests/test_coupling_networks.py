23ae34ab8207a4f4d8beff7af1ff4cc9ea05c54f
stefanradev93 <stefan.radev93@gmail.com>
2022-11-08 17:34:12 +0100

    Reduce number of tests using randomization

--- tests/test_coupling_networks.py
+++ tests/test_coupling_networks.py
@@ -27,24 +27,28 @@ from bayesflow.helper_functions import build_meta_dict
 from bayesflow.default_settings import DEFAULT_SETTING_INVERTIBLE_NET
 
 
-@pytest.mark.parametrize("input_dim", [2, 5])
-@pytest.mark.parametrize("condition_dim", [8, None])
-@pytest.mark.parametrize("units", [16, 8])
+@pytest.mark.parametrize("condition", [True, False])
 @pytest.mark.parametrize("spec_norm", [True, False])
 @pytest.mark.parametrize("use_perm", [True, False])
 @pytest.mark.parametrize("use_act_norm", [True, False])
-def test_coupling_layer(input_dim, condition_dim, units, spec_norm, use_perm, use_act_norm):
+@pytest.mark.parametrize("input_shape", ['2d', '3d'])
+def test_coupling_layer(condition, spec_norm, use_perm, use_act_norm, input_shape):
     """Tests the `CouplingLayer` instance with various configurations."""
+
+    # Randomize units and input dim
+    units_t = np.random.randint(low=2, high=32)
+    units_s = np.random.randint(low=2, high=32)
+    input_dim = np.random.randint(low=2, high=32)
 
     # Create settings dictionaries and network
     dense_net_settings = {
         't_args': {
-            'dense_args': dict(units=units, kernel_initializer='glorot_uniform', activation='elu'),
+            'dense_args': dict(units=units_t, kernel_initializer='glorot_uniform', activation='elu'),
             'n_dense': 2,
             'spec_norm': spec_norm
         },
         's_args': {
-            'dense_args': dict(units=units, kernel_initializer='glorot_uniform', activation='elu'),
+            'dense_args': dict(units=units_s, kernel_initializer='glorot_uniform', activation='elu'),
             'n_dense': 1,
             'spec_norm': spec_norm
         },
@@ -61,14 +65,20 @@ def test_coupling_layer(input_dim, condition_dim, units, spec_norm, use_perm, us
 
     # Create randomized input and output conditions
     batch_size = np.random.randint(low=1, high=32)
-    inp = np.random.normal(size=(batch_size, input_dim)).astype(np.float32)
-    if condition_dim is None:
-        condition = None
+    if input_shape == '2d':
+        inp = np.random.normal(size=(batch_size, input_dim)).astype(np.float32)
     else:
+        n_obs = np.random.randint(low=1, high=32)
+        inp = np.random.normal(size=(batch_size, n_obs, input_dim)).astype(np.float32)
+    if condition :
+        condition_dim = np.random.randint(low=1, high=32)
         condition = np.random.normal(size=(batch_size, condition_dim)).astype(np.float32)
+    else:
+        condition = None
 
     # Forward and inverse pass
     z, ldj = network(inp, condition)
+    z = z.numpy()
     inp_rec = network(z, condition, inverse=True).numpy()
 
     # Test attributes
@@ -79,5 +89,8 @@ def test_coupling_layer(input_dim, condition_dim, units, spec_norm, use_perm, us
     # Test invertibility
     assert np.allclose(inp, inp_rec, atol=1e6)
     # Test shapes (bijectivity)
-    assert z.shape[0] == inp.shape[0] and z.shape[1] == inp.shape[1]
-    assert ldj.shape[0] == inp.shape[0]
+    assert z.shape == inp.shape
+    if input_shape == '2d':
+        assert ldj.shape[0] == inp.shape[0]
+    else:
+        assert ldj.shape[0] == inp.shape[0] and ldj.shape[1] == inp.shape[1]
