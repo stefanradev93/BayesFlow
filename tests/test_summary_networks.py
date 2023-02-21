c81eec3e04bab17d3027e632352ca27e99fd0874
stefanradev93 <stefan.radev93@gmail.com>
2023-02-21 20:06:37 +0100

    Add test of set transformer

--- tests/test_summary_networks.py
+++ tests/test_summary_networks.py
@@ -21,7 +21,8 @@
 import numpy as np
 import pytest
 
-from bayesflow.summary_networks import InvariantNetwork, SequentialNetwork
+from bayesflow.attention import InducedSelfAttentionBlock, SelfAttentionBlock
+from bayesflow.summary_networks import DeepSet, SequentialNetwork, SetTransformer, TimeSeriesTransformer
 
 
 def _gen_randomized_3d_data(low=1, high=32, dtype=np.float32):
@@ -49,13 +50,13 @@ def _gen_randomized_3d_data(low=1, high=32, dtype=np.float32):
 
 @pytest.mark.parametrize("num_equiv", [1, 3])
 @pytest.mark.parametrize("summary_dim", [13, 2])
-def test_invariant_network(num_equiv, summary_dim):
-    """This function tests the fidelity of the ``InvariantNetwork`` with a couple of relevant
+def test_deep_set(num_equiv, summary_dim):
+    """This function tests the fidelity of the ``DeepSet`` with a couple of relevant
     configurations w.r.t. permutation invariance and output dimensions."""
 
-    # Prepare settings for invariant network
+    # Prepare settings for the deep set
     settings = {"num_equiv": num_equiv, "summary_dim": summary_dim}
-    inv_net = InvariantNetwork(**settings)
+    inv_net = DeepSet(**settings)
 
     # Create input and permuted version with randomized shapes
     x, x_perm, _ = _gen_randomized_3d_data()
@@ -99,3 +100,50 @@ def test_sequential_network(num_conv_layers, lstm_units):
     assert out.shape[1] == settings["summary_dim"]
     # Test first dimension unaltered
     assert out.shape[0] == x.shape[0]
+
+
+@pytest.mark.parametrize("summary_dim", [13, 4])
+@pytest.mark.parametrize("num_seeds", [1, 3])
+@pytest.mark.parametrize("num_attention_blocks", [1, 2])
+@pytest.mark.parametrize("num_inducing_points", [None, 4])
+def test_set_transformer(summary_dim, num_seeds, num_attention_blocks, num_inducing_points):
+    """This function tests the fidelity of the ``SetTransformer`` with a couple of relevant
+    configurations w.r.t. permutation invariance and output dimensions."""
+
+    # Prepare settings for transformer
+    att_dict = {"num_head": 2, "key_dim": 16}
+    dense_dict = {"units": 16, "activation": "relu"}
+
+    # Create input and permuted version with randomized shapes
+    x, x_perm, _ = _gen_randomized_3d_data()
+
+    transformer = SetTransformer(
+        input_dim=x.shape[2],
+        attention_settings=att_dict,
+        dense_settings=dense_dict,
+        summary_dim=summary_dim,
+        num_attention_blocks=num_attention_blocks,
+        num_inducing_points=num_inducing_points,
+        num_seeds=num_seeds,
+    )
+
+    # Pass unpermuted and permuted inputs
+    out = transformer(x).numpy()
+    out_perm = transformer(x_perm).numpy()
+
+    # Assert numebr of equivariant layers correct
+    assert len(transformer.attention_blocks.layers) == num_attention_blocks
+    # Assert outputs equal
+    assert np.allclose(out, out_perm, atol=1e-5)
+    # Assert shape 2d
+    assert len(out.shape) == 2 and len(out_perm.shape) == 2
+    # Assert batch and last dimension equals output dimension
+    assert x.shape[0] == out.shape[0] and x_perm.shape[0] == out.shape[0]
+    out_dim = int(num_seeds * summary_dim)
+    assert out.shape[1] == out_dim and out_perm.shape[1] == out_dim
+    # Assert type of attention layer
+    for block in transformer.attention_blocks.layers:
+        if num_inducing_points is None:
+            assert type(block) is SelfAttentionBlock
+        else:
+            assert type(block) is InducedSelfAttentionBlock
