3f0796650f02341ea364691f6b7695a683e90eb8
stefanradev93 <stefan.radev93@gmail.com>
2023-03-02 17:12:28 +0100

    generalize logloss

--- bayesflow/losses.py
+++ bayesflow/losses.py
@@ -136,16 +136,31 @@ def mmd_summary_space(summary_outputs, z_dist=tf.random.normal, kernel="gaussian
     return mmd_loss
 
 
-def log_loss(model_indices, alpha, label_smoothing=0.01):
-    """Computes the logloss given output probs and true model indices m_true.
+def log_loss(model_indices, preds, evidential=False, label_smoothing=0.01):
+    """Computes the logarithmic loss given true ``model_indices`` and approximate model
+    probabilities either according to [1] if ``evidential is True`` or according to [2]
+    if ``evidential is False``.
+
+    [1] Radev, S. T., D'Alessandro, M., Mertens, U. K., Voss, A., Köthe, U., & Bürkner, P. C. (2021).
+    Amortized bayesian model comparison with evidential deep learning.
+    IEEE Transactions on Neural Networks and Learning Systems.
+
+    [2] Elsemüller, L., Schnuerch, M., Bürkner, P. C., & Radev, S. T. (2023).
+    A Deep Learning Method for Comparing Bayesian Hierarchical Models.
+    arXiv preprint arXiv:2301.11873.
 
     Parameters
     ----------
-    model_indices : tf.Tensor of shape (batch_size, n_models)
+    model_indices   : tf.Tensor of shape (batch_size, num_models)
         one-hot-encoded true model indices
-    alpha         : tf.Tensor of shape (batch_size, n_models)
-        positive network outputs in ``[1, +inf]``
-    label_smoothing : float or None, optional, default: 0.05
+    preds           : tf.Tensor of shape (batch_size, num_models)
+        If ``evidential is True`` these should be the concentration
+        parameters of a Dirichlet density bounded between ``[1, +inf]``.
+        Else, these should be normalized probability values.
+    evidential      : boolean, optional, default: False
+        Whether to first normalize ``preds`` (True) or assume
+        normalized (False, default)
+    label_smoothing : float or None, optional, default: 0.01
         Optional label smoothing factor.
 
     Returns
@@ -160,12 +175,13 @@ def log_loss(model_indices, alpha, label_smoothing=0.01):
         model_indices *= 1.0 - label_smoothing
         model_indices += label_smoothing / num_models
 
-    # Obtain probs
-    model_probs = alpha / tf.reduce_sum(alpha, axis=1, keepdims=True)
+    # Obtain probs if using an evidential network
+    if evidential:
+        preds = preds / tf.reduce_sum(preds, axis=1, keepdims=True)
 
     # Numerical stability
-    model_probs = tf.clip_by_value(model_probs, 1e-15, 1 - 1e-15)
+    preds = tf.clip_by_value(preds, 1e-15, 1 - 1e-15)
 
     # Actual loss + regularization (if given)
-    loss = -tf.reduce_mean(tf.reduce_sum(model_indices * tf.math.log(model_probs), axis=1))
+    loss = -tf.reduce_mean(tf.reduce_sum(model_indices * tf.math.log(preds), axis=1))
     return loss
