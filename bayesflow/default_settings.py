41c36f2dae4e4366d6db68f8b17ade2099214230
stefanradev93 <stefan.radev93@gmail.com>
2023-03-31 15:22:10 +0200

    Update w decay as in master [skip ci]

--- bayesflow/default_settings.py
+++ bayesflow/default_settings.py
@@ -98,7 +98,7 @@ DEFAULT_SETTING_DENSE_PMP = {
 
 DEFAULT_SETTING_AFFINE_COUPLING = MetaDictSetting(
     meta_dict={
-        "dense_args": dict(units=128, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(5e-3)),
+        "dense_args": dict(units=128, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(5e-4)),
         "num_dense": 2,
         "spec_norm": False,
         "mc_dropout": False,
