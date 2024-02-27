0b5d9c697fb266eaf89924250a0df42a5a03d2d9
LarsKue <lars@kuehmichel.de>
2024-02-27 17:32:56 +0100

    add imports

--- bayesflow/experimental/backend_agnostic/__init__.py
+++ bayesflow/experimental/backend_agnostic/__init__.py
@@ -1,3 +1,10 @@
 
+from .generative_model import GenerativeModel
+from .sampling import (
+    SampleContextsMixin,
+    SampleLikelihoodMixin,
+    SampleParametersMixin,
+    SamplePosteriorMixin,
+    SampleObservationsMixin,
+)
 from .types import Tensor, Shape
-
