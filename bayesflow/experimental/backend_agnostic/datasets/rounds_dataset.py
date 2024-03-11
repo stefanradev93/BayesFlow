953ba92da553918d24286b71042591bd4e34881d
LarsKue <lars@kuehmichel.de>
2024-03-11 17:37:37 +0100

    fix rounds dataset

--- bayesflow/experimental/backend_agnostic/datasets/rounds_dataset.py
+++ bayesflow/experimental/backend_agnostic/datasets/rounds_dataset.py
@@ -6,13 +6,15 @@ from bayesflow.experimental.backend_agnostic.simulation.generative_model import 
 
 class RoundsDataset(keras.utils.PyDataset):
     """
-    A dataset that is generated on-the-fly at the beginning of each epoch.
+    A dataset that is generated on-the-fly at the beginning of every n-th epoch.
     """
-    def __init__(self, generative_model: GenerativeModel, batch_size: int, batches_per_epoch: int, **kwargs):
+    def __init__(self, generative_model: GenerativeModel, batch_size: int, batches_per_epoch: int, epochs_per_round: int, **kwargs):
         super().__init__(**kwargs)
         self.generative_model = generative_model
         self.batch_size = batch_size
         self.batches_per_epoch = batches_per_epoch
+        self.epochs_per_round = epochs_per_round
+        self.epoch = 0
 
         self.data = None
 
@@ -27,7 +29,9 @@ class RoundsDataset(keras.utils.PyDataset):
         return self.batches_per_epoch
 
     def on_epoch_end(self) -> None:
-        self.regenerate()
+        self.epoch += 1
+        if self.epoch % self.epochs_per_round == 0:
+            self.regenerate()
 
     def regenerate(self) -> None:
         """ Sample batches of data from the generative model """
