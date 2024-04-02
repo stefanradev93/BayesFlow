a63afbc67f69b08f2000eda78aa8cc9455f04da1
Lars <lars@kuehmichel.de>
2024-04-02 10:46:24 +0200

    remove migration module

--- bayesflow/migration/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-
-This module implements migration utilities to ease transitions between different versions of the package API.
-
-"""
-
-from .context import ContextGenerator
-from .prior import Prior
-from .simulator import Simulator
