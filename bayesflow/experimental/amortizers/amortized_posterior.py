548b943bdc5d7be1fa9145eb0a9cac9815f4fc4e
lars <lars@kuehmichel.de>
2024-05-10 17:07:18 +0200

    adjust amortizer concat dim

--- bayesflow/experimental/amortizers/amortized_posterior.py
+++ bayesflow/experimental/amortizers/amortized_posterior.py
@@ -6,10 +6,10 @@ from .amortizer import Amortizer
 
 class AmortizedPosterior(Amortizer):
     def configure_inferred_variables(self, data: dict):
-        return keras.ops.concatenate(list(data["parameters"].values()), axis=1)
+        return keras.ops.concatenate(list(data["parameters"].values()), axis=-1)
 
     def configure_observed_variables(self, data: dict):
-        return keras.ops.concatenate(list(data["observables"].values()), axis=1)
+        return keras.ops.concatenate(list(data["observables"].values()), axis=-1)
 
     def configure_inference_conditions(self, data: dict, summary_outputs=None):
         if summary_outputs is None:
