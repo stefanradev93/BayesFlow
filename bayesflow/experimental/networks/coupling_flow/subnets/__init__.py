7519f86dbc3869d926fc0a607ebf24c3cb0460ac
LarsKue <lars@kuehmichel.de>
2024-05-28 19:24:09 +0200

    adjust find_subnet to lazy initialization

--- bayesflow/experimental/networks/coupling_flow/subnets/__init__.py
+++ bayesflow/experimental/networks/coupling_flow/subnets/__init__.py
@@ -1,25 +1,25 @@
 
-# TODO: This is a temporary solution to avoid circular imports.
-#  The correct solution is to move the subnets to a separate module.
-#  We should also probably use "ResNet" here
 from typing import Callable
 
-from ..transforms import Transform
-from ...resnet.residual_block import ConditionalResidualBlock
+import keras
+
+import bayesflow.experimental.networks as networks
 
 
-def find_subnet(subnet: str | Callable, transform: Transform, output_dim: int, **kwargs):
-
+def find_subnet(subnet: str | keras.Layer | Callable, **kwargs) -> keras.Layer:
+    """ Find subnetworks by name and configure them to use lazy in- and output dimensions. """
     match subnet:
         case str() as name:
             match name.lower():
-                case "default":
-                    constructor = ConditionalResidualBlock
+                case "resnet":
+                    resnet = networks.ResNet.new(**kwargs)
+                    resnet.output_layer.units = None
+                    return resnet
                 case other:
                     raise NotImplementedError(f"Unsupported subnet name: '{other}'.")
-        case callable():
-            constructor = subnet
+        case keras.Layer() as layer:
+            return layer
+        case callable() as constructor:
+            return constructor(**kwargs)
         case other:
-            raise NotImplementedError(f"Cannot infer subnet constructor from {other!r}.")
-    output_dim = output_dim * transform.params_per_dim
-    return constructor(output_dim, **kwargs)
+            raise NotImplementedError(f"Cannot infer subnet from {other!r}.")
