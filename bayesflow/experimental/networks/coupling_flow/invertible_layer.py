3f641a0dcf76b94faf118393ff83cd0d02808073
LarsKue <lars@kuehmichel.de>
2024-05-28 19:24:52 +0200

    add base InvertibleLayer

---
+++ bayesflow/experimental/networks/coupling_flow/invertible_layer.py
@@ -0,0 +1,16 @@
+
+import keras
+
+
+class InvertibleLayer(keras.Layer):
+    def call(self, *args, **kwargs):
+        # we cannot provide a default implementation for this
+        #  because the signature of layer.call() is used to
+        #  determine the arguments to layer.build()
+        raise NotImplementedError
+
+    def _forward(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _inverse(self, *args, **kwargs):
+        raise NotImplementedError
