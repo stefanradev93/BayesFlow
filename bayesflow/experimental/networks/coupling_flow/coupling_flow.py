6523d3123142406c5221afe56cd47ce5dec54f98
LarsKue <lars@kuehmichel.de>
2024-05-31 09:30:51 +0200

    fix log_det shape incompatibility with actnorm

--- bayesflow/experimental/networks/coupling_flow/coupling_flow.py
+++ bayesflow/experimental/networks/coupling_flow/coupling_flow.py
@@ -62,7 +62,7 @@ class CouplingFlow(InferenceNetwork):
 
     def _forward(self, x: Tensor, jacobian: bool = False, **kwargs) -> Union[Tensor, Tuple[Tensor, Tensor]]:
         z = x
-        log_det = 0.0
+        log_det = keras.ops.zeros((keras.ops.shape(x)[0],))
         for layer in self._layers:
             z, det = layer(z, inverse=False, **kwargs)
             log_det += det
@@ -73,7 +73,7 @@ class CouplingFlow(InferenceNetwork):
 
     def _inverse(self, z: Tensor, jacobian: bool = False, **kwargs) -> Union[Tensor, Tuple[Tensor, Tensor]]:
         x = z
-        log_det = 0.0
+        log_det = keras.ops.zeros((keras.ops.shape(z)[0],))
         for layer in reversed(self._layers):
             x, det = layer(x, inverse=True, **kwargs)
             log_det += det
