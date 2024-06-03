9a0fc437f0ef863ef6abba47194f7b32d1654fb4
lars <lars@kuehmichel.de>
2024-06-03 15:36:31 +0200

    add work-around to https://github.com/keras-team/keras/issues/19796 in tests

--- tests/utils/assertions.py
+++ tests/utils/assertions.py
@@ -17,6 +17,11 @@ def assert_layers_equal(layer1: keras.Layer, layer2: keras.Layer):
     assert len(layer1.variables) == len(layer2.variables), f"Layers {layer1.name} and {layer2.name} have a different number of variables ({len(layer1.variables)}, {len(layer2.variables)})."
     assert len(layer1.variables) > 0, f"Layers {layer1.name} and {layer2.name} have no variables."
     for v1, v2 in zip(layer1.variables, layer2.variables):
+
+        if v1.name == "seed_generator_state" and v1.name == v2.name:
+            # keras issue: https://github.com/keras-team/keras/issues/19796
+            continue
+
         v1 = keras.ops.convert_to_numpy(v1)
         v2 = keras.ops.convert_to_numpy(v2)
         assert keras.ops.all(keras.ops.isclose(v1, v2)), f"Variables for {layer1.name} and {layer2.name} are not equal: {v1} != {v2}"
