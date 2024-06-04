50030e7fd824fa5d7248a36fcef09600a75ecfba
stefanradev93 <stefan.radev93@gmail.com>
2024-06-04 08:06:49 -0400

    Add keras_kwargs utility

--- bayesflow/experimental/utils/dictutils.py
+++ bayesflow/experimental/utils/dictutils.py
@@ -8,3 +8,10 @@ def nested_getitem(data: dict, item: int) -> dict:
         else:
             result[key] = value[item]
     return result
+
+
+def keras_kwargs(kwargs: dict):
+    """ Keep dictionary keys that do not end with _kwargs. Used for propagating
+    custom keyword arguments in custom models that inherit from keras.Model.
+    """
+    return {key: value for key, value in kwargs.items() if not key.endswith("_kwargs")}
