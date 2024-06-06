af3b6f493e4a9d99ad88ead9340a5e199c807962
Chase-Grajeda <chase.grajeda@gmail.com>
2024-06-06 09:57:01 -0400

    Conv1 Edits
    Added conv1 custom args and placeholder for GRU model

--- bayesflow/experimental/networks/lstnet/lstnet.py
+++ bayesflow/experimental/networks/lstnet/lstnet.py
@@ -1,7 +1,7 @@
 import keras
 from bayesflow.experimental.types import Tensor
 from bayesflow.experimental.utils import keras_kwargs
-from keras import layers, Sequential
+from keras import layers, Sequential, regularizers
 from keras.saving import (register_keras_serializable)
 
 @register_keras_serializable(package="bayesflow.networks") #TODO: finalize class name and add to package string
@@ -13,32 +13,44 @@ class LSTNet(keras.Model):
     2023 IEEE/ASME International Conference on Advanced Intelligent Mechatronics (AIM), 
     Seattle, WA, USA, 2023, pp. 966-972, doi: 10.1109/AIM46323.2023.10196190.
     
+    TODO: Add class spec and arg descriptions
+    
     """
     
     # TODO: use new kwargs system
     
     def __init__(
         self,
+        num_filters: int,
         kernel_size: int = 4,
+        kernel_initializer: str = "glorot_uniform",
+        kernel_regularizer: regularizers.Regularizer | None = None,
         activation: str = "relu",
         **kwargs
     ):
         super().__init__(**kwargs)
         
-        # Define layers here
+        # TODO: Tidy code and condense comments
+        
+        # Define model sequencer
         model = Sequential()
-        
-        # Convolution (with relu) and dense layers (for debugging)
-        
+                
         # 1D convolution layer with ReLU activation
-        conv1 = layers.Conv1D(activation="relu")
+        conv1 = layers.Conv1D(
+            filters=num_filters,
+            kernel_size=kernel_size,
+            activation="relu",
+            kernel_initializer=kernel_initializer,
+            kernel_regularizer=kernel_regularizer
+        )
         
         # Batch normalization layer
-        bnorm = layers.BatchNormalization(...)
+        bnorm = layers.BatchNormalization() # any custom ars here? 
         
-        # GRU layers
-        full_gru = layers.GRU(...)
+        # GRU layers (create some sort of functional to add to Sequencer)
+        full_gru = layers.GRU(...) # temp for gru.py
         skip_gru = layers.GRU(...)
+        comb_gru = layers.Concatenate(axis=-1)
         
         # Final dense layer
         final_dense = layers.Dense(...)
@@ -47,7 +59,7 @@ class LSTNet(keras.Model):
         model.add(conv1)
         model.add(bnorm)
         
-        # How to send to different channels?
+        # How to send to different channels for GRU(s)?
         
         model.add(final_dense)
         
