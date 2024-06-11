67b6726386e2619c2a05561f14da4757f6f16d1c
Chase-Grajeda <chase.grajeda@gmail.com>
2024-06-11 12:44:56 -0400

    WIP
    Condensed layers to sequential model.
    Removed reshaping layers as they are no longer necessary.

--- bayesflow/experimental/networks/lstnet/lstnet.py
+++ bayesflow/experimental/networks/lstnet/lstnet.py
@@ -1,11 +1,11 @@
 import keras
 # from bayesflow.experimental.types import Tensor
-from tensorflow import Tensor
+from tensorflow import Tensor # TODO: remove, temp to stop strange Notebook errors
 from bayesflow.experimental.utils import keras_kwargs
 from keras import layers, Sequential, regularizers
 from keras.saving import (register_keras_serializable)
 
-@register_keras_serializable(package="bayesflow.networks.lstnet") #TODO: finalize class name and add to package string
+@register_keras_serializable(package="bayesflow.networks.lstnet")
 class LSTNet(keras.Model):
     """
     Implements a LSTNet Architecture as described in [1]
@@ -19,8 +19,6 @@ class LSTNet(keras.Model):
     """
     
     # TODO: use new kwargs system
-    # TODO: currently assumes cnn output dim will match gru output dim. Do these have to be different?
-    # TODO: maybe change conv_out to hidden_dim, and add a separate out_dim required variable
     
     def __init__(
         self,
@@ -30,8 +28,9 @@ class LSTNet(keras.Model):
         kernel_initializer: str = "glorot_uniform",
         kernel_regularizer: regularizers.Regularizer | None = None,
         activation: str = "relu",
-        conv_out: int = 64,
-        skip_step: int = 6, # S
+        gru_out: int = 64,
+        dense_out: int = 64,
+        skip_steps: list[int] = [1], # S
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -53,48 +52,28 @@ class LSTNet(keras.Model):
         # Batch normalization layer
         self.bnorm = layers.BatchNormalization() # TODO: any custom args here?
         
-        # Reshaping layers
-        # TODO: skeptical on these dimensions being correct
-        self.keep_reshape = layers.Reshape((-1, num_time_steps - kernel_size + 1, conv_out))
-        self.skip_reshape = layers.Reshape((-1, (num_time_steps - kernel_size + 1) // skip_step, conv_out * skip_step))
-        
         # GRU layers
-        self.keep_gru = layers.GRU(conv_out) # temp for gru.py
-        self.skip_gru = layers.GRU(conv_out) # temp for skip_gru.py
+        self.keep_gru = layers.GRU(gru_out) # temp for gru.py
+        self.skip_gru = layers.GRU(gru_out) # temp for skip_gru.py
         self.gru_add = layers.Add()
         # self.gru_concat = layers.Concatenate(axis=-1)
         
         # Final dense layer
-        self.final_dense = layers.Dense(cnn_out, activation="relu") # TODO: upgrade to ResNet
+        self.final_dense = layers.Dense(dense_out, activation="relu") # TODO: upgrade to ResNet
         
         # Aggregate layers
-        # self.model.add(conv1)
-        # self.model.add(bnorm)
-        
-        # How to send to different channels for GRU(s)?
+        #                                In:  (batch, time steps, num series)
+        self.model.add(self.conv1)       # -> (batch, reduced time steps, cnn_out)
+        self.model.add(self.bnorm)       # -> (batch, reduced time steps, cnn_out)
+        self.model.add(self.keep_gru)    # -> (batch, gru_out)
+        self.model.add(self.final_dense) # -> (batch, gru_out)
+    
     
     def call(self, x: Tensor) -> Tensor:
-        print("Performing pass with shape: ", x.shape)
-        
-        # Conv and batch norm
-        # Input: (2, 32, 2)
-        x1 = self.conv1(x) # (2, 29, 32)
-        x2 = self.bnorm(x1) # (2, 29, 32)
-        # x = self.model(x)
-        # Reshape into parallel channels
-        x_keep = self.keep_reshape(x2)
-        x_skip = self.skip_reshape(x2)
-        # Parallel GRU and Skip GRU
-        x_keep = self.keep_gru(x_keep)
-        x_skip = self.skip_gru(x_skip)
-        # Concatenate results
-        x3 = self.gru_add([x_keep, x_skip])
-        # Final dense and return
-        x4 = self.final_dense(x3)
-        return x4
+        x = self.model(x)
+        return x
     
     
     def build(self, input_shape):
-        # Built in reference to deepset changes
         super().build(input_shape)
         self(keras.KerasTensor(input_shape))
