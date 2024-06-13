a604d99a780fbd11796834e43128417c817f45a9
Chase-Grajeda <chase.grajeda@gmail.com>
2024-06-13 10:40:09 -0400

    Removed gru.py
    Removed gru.py since keras has it's own implementation

--- bayesflow/experimental/networks/lstnet/gru.py
+++
@@ -1,8 +0,0 @@
-import keras
-from keras.saving import register_keras_serializable
-from bayesflow.experimental.types import Tensor
-
-@register_keras_serializable(package="bayesflow.networks")
-class GRU(keras.Model):
-    # TODO
-    pass
