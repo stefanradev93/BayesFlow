a51e9ac82eea477cda49626dfecdf89862947851
larskue <lars@kuehmichel.de>
2024-06-13 18:16:20 +0200

    adjust tests for batched_call simulators also activate tests

--- tests/test_datasets/test_datasets.py
+++ tests/test_datasets/test_datasets.py
@@ -4,32 +4,29 @@ import pickle
 import pytest
 
 
-@pytest.mark.skip(reason="WIP")
 def test_dataset_is_picklable(dataset):
     pickled = pickle.loads(pickle.dumps(dataset))
 
     assert type(pickled) is type(dataset)
 
-    samples = dataset[0]  # tuple of (x, y)
-    samples = samples[0]  # dict of {param_name: param_value}
+    samples = dataset[0]  # dict of {param_name: param_value}
     samples = next(iter(samples.values()))  # first param value
 
     pickled_samples = pickled[0]
-    pickled_samples = pickled_samples[0]
     pickled_samples = next(iter(pickled_samples.values()))
 
     assert keras.ops.shape(samples) == keras.ops.shape(pickled_samples)
 
 
-@pytest.mark.skip(reason="WIP")
+# TODO: implement jax train loop
+@pytest.mark.torch
+@pytest.mark.tensorflow
 def test_dataset_works_in_fit(model, dataset):
     model.fit(dataset, epochs=1, steps_per_epoch=1)
 
 
-@pytest.mark.skip(reason="WIP")
 def test_dataset_returns_batch(dataset, batch_size):
-    samples = dataset[0]  # tuple of (x, y)
-    samples = samples[0]  # dict of {param_name: param_value}
+    samples = dataset[0]  # dict of {param_name: param_value}
     samples = next(iter(samples.values()))  # first param value
 
     assert keras.ops.shape(samples)[0] == batch_size
