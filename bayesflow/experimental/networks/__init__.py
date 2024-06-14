e6a1cdefee2251490575249b4f091f1f65fc767a
stefanradev93 <stefan.radev93@gmail.com>
2024-06-14 06:30:11 -0400

    Expose LSTNet

--- bayesflow/experimental/networks/__init__.py
+++ bayesflow/experimental/networks/__init__.py
@@ -4,6 +4,7 @@ from .deep_set import DeepSet
 from .flow_matching import FlowMatching
 from .mlp import MLP
 from .resnet import ResNet
+from .lstnet import LSTNet
 from .transformers import SetTransformer
 
 from .inference_network import InferenceNetwork
