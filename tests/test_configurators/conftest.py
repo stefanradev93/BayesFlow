e2355de3c6ea678a4e8eaa6e089a7e67ccea98d6
Chase Grajeda <76405306+Chase-Grajeda@users.noreply.github.com>
2024-06-18 22:15:37 +0900

    Configurator Unit Tests (#174)
    * First additions
    
    Added __init__.py for test module.
    Added test_configurators.py.
    Added basic fixtures and construction tests.
    
    * Remaining tests
    
    Added remaining unit tests
    
    * Added conftest
    
    Separated fixtures and placed them in conftest.py
    
    * Added requested changes
    
    Added batch_size, set_size, and num_features parameterizations in conftest.py.
    Combined repetitive fixtures in conftest.py.
    Combined repetitive tests in test_configurators.py.
    Parameterized Configurator initialization in conftest.py.
    Parameterized parameter selection in conftest.py.
    Removed initialization tests in test_configurators.py.
    Added summary_inputs and summary_conditions to parameters.
    Changed instances of '==None' to 'is None'.
    Removed 'config=Configurator' instances in test_configurators.py.

---
+++ tests/test_configurators/conftest.py
@@ -0,0 +1,59 @@
+import keras
+import pytest
+from bayesflow.experimental.configurators import Configurator
+
+
+@pytest.fixture(params=[2, 3])
+def batch_size(request):
+    return request.param
+
+
+@pytest.fixture(params=[2, 3])
+def set_size(request):
+    return request.param
+
+
+@pytest.fixture(params=[2,3])
+def num_features(request):
+    return request.param
+
+
+@pytest.fixture(params=[True, False])
+def random_data(request, batch_size, set_size, num_features):
+    data = {
+        "var1": keras.random.normal((batch_size, set_size, num_features)),
+        "var2": keras.random.normal((batch_size, set_size, num_features)),
+        "var3": keras.random.normal((batch_size, set_size, num_features)),
+        "summary_inputs": keras.random.normal((batch_size, set_size, num_features)),
+        "summary_conditions": keras.random.normal((batch_size, set_size, num_features))
+    }
+    if request.param:
+        data["summary_outputs"] = keras.random.normal((batch_size, set_size, num_features))
+    return data
+
+
+@pytest.fixture(params=[True, False])
+def test_params(request):
+    args = {
+        "inference_variables": ["var1"],
+        "inference_conditions": ["var2", "var3"],
+        "summary_variables": ["var1"],
+        "summary_conditions": ["var2"]
+    }
+    if request.param:
+        args["inference_conditions"].append("summary_outputs")
+    return args
+
+
+@pytest.fixture(params=[True, False])
+def configurator(request, test_params):
+    if request.param:
+        return Configurator(
+            inference_variables=test_params["inference_variables"]
+        )
+    return Configurator(
+        inference_variables=test_params["inference_variables"],
+        inference_conditions=test_params["inference_conditions"],
+        summary_variables=test_params["summary_variables"],
+        summary_conditions=test_params["summary_conditions"]
+    )
