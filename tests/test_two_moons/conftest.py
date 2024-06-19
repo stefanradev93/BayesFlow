09a76a9ba861d6e431829a286a5aa8e380f72225
lars <lars@kuehmichel.de>
2024-06-19 14:28:41 +0200

    reorganize fixtures

--- tests/test_two_moons/conftest.py
+++ tests/test_two_moons/conftest.py
@@ -6,8 +6,24 @@ import pytest
 
 
 @pytest.fixture()
+def approximator(inference_network):
+    from bayesflow.experimental.backend_approximators import Approximator
+    return Approximator(
+        inference_network=inference_network,
+        inference_variables=["theta"],
+        inference_conditions=["x", "r", "alpha"],
+    )
+
+
+@pytest.fixture()
 def batch_size():
     return 32
+
+
+@pytest.fixture()
+def inference_network():
+    from bayesflow.experimental.networks import CouplingFlow
+    return CouplingFlow()
 
 
 @pytest.fixture()
@@ -35,21 +51,6 @@ def simulator():
 
     return simulator
 
-    # class Simulator:
-    #     def sample(self, batch_shape):
-    #         r = keras.random.normal(shape=batch_shape + (1,), mean=0.1, stddev=0.01)
-    #         alpha = keras.random.uniform(shape=batch_shape + (1,), minval=-0.5 * math.pi, maxval=0.5 * math.pi)
-    #         theta = keras.random.uniform(shape=batch_shape + (2,), minval=-1.0, maxval=1.0)
-    #
-    #         x1 = -keras.ops.abs(theta[..., :1] + theta[..., 1:]) / keras.ops.sqrt(2.0) + r * keras.ops.cos(alpha) + 0.25
-    #         x2 = (-theta[..., :1] + theta[..., 1:]) / keras.ops.sqrt(2.0) + r * keras.ops.sin(alpha)
-    #
-    #         x = keras.ops.concatenate([x1, x2], axis=-1)
-    #
-    #         return dict(r=r, alpha=alpha, theta=theta, x=x)
-    #
-    # return Simulator()
-
 
 @pytest.fixture()
 def train_dataset(simulator, batch_size):
@@ -63,19 +64,3 @@ def validation_dataset(simulator, batch_size):
     from bayesflow.experimental.datasets import OfflineDataset
     data = simulator.sample((4 * batch_size,))
     return OfflineDataset(data, workers=4, max_queue_size=16, batch_size=batch_size)
-
-
-@pytest.fixture()
-def inference_network():
-    from bayesflow.experimental.networks import CouplingFlow
-    return CouplingFlow()
-
-
-@pytest.fixture()
-def approximator(inference_network):
-    from bayesflow.experimental.backend_approximators import Approximator
-    return Approximator(
-        inference_network=inference_network,
-        inference_variables=["theta"],
-        inference_conditions=["x", "r", "alpha"],
-    )
