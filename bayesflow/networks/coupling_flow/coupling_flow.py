793eaa383aae05767a39559748539cd5a178da6e
stefanradev93 <stefan.radev93@gmail.com>
2024-06-20 09:02:42 -0400

    Change order to reflect theory and papers -> rotation then coupling then actnorm

--- bayesflow/networks/coupling_flow/coupling_flow.py
+++ bayesflow/networks/coupling_flow/coupling_flow.py
@@ -39,7 +39,7 @@ class CouplingFlow(InferenceNetwork):
     def __init__(
         self,
         depth: int = 6,
-        subnet: str = "resnet",
+        subnet: str = "default",
         transform: str = "affine",
         permutation: str | None = None,
         use_actnorm: bool = True,
@@ -52,13 +52,14 @@ class CouplingFlow(InferenceNetwork):
 
         self.invertible_layers = []
         for i in range(depth):
-            if use_actnorm:
-                self.invertible_layers.append(ActNorm(**kwargs))
-
-            self.invertible_layers.append(DualCoupling(subnet, transform, **kwargs))
 
             if (p := find_permutation(permutation, **kwargs)) is not None:
                 self.invertible_layers.append(p)
+
+            self.invertible_layers.append(DualCoupling(subnet, transform, **kwargs))
+
+            if use_actnorm:
+                self.invertible_layers.append(ActNorm(**kwargs))
 
     # noinspection PyMethodOverriding
     def build(self, xz_shape, conditions_shape=None):
