14b0deaa8355e8180fd0d2dcd1b571327d88db78
larskue <lars@kuehmichel.de>
2024-07-08 17:08:05 +0200

    turn off torch autograd by default for consistency with other backends

--- bayesflow/__init__.py
+++ bayesflow/__init__.py
@@ -11,3 +11,15 @@ from . import (
 
 from .approximators import Approximator
 from .datasets import OfflineDataset, OnlineDataset
+
+import keras
+
+if keras.backend.backend() == "torch":
+    # turn off gradients by default
+    import torch
+
+    torch.autograd.set_grad_enabled(False)
+
+    # clean up namespace
+    del keras
+    del torch
