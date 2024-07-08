3b02ec886292d9bd0964eb536aab4ce0bc38e207
larskue <lars@kuehmichel.de>
2024-07-08 14:57:24 +0200

    add simulator.rejection_sample()

--- bayesflow/simulators/simulator.py
+++ bayesflow/simulators/simulator.py
@@ -1,6 +1,36 @@
+import keras
+
 from bayesflow.types import Shape, Tensor
+from bayesflow.utils import concatenate_dicts
 
 
 class Simulator:
-    def sample(self, batch_shape: Shape, **kwargs) -> dict[str, Tensor]:
+    def sample(self, batch_shape: Shape, *, numpy: bool = False, **kwargs) -> dict[str, Tensor]:
         raise NotImplementedError
+
+    def rejection_sample(
+        self, batch_shape: Shape, condition: callable, *, axis: int = 0, numpy: bool = False, **kwargs
+    ) -> dict[str, Tensor]:
+        result = {}
+
+        while not result or keras.ops.shape(next(iter(result.values())))[axis] < batch_shape[axis]:
+            samples = self.sample(batch_shape, **kwargs)
+            accept_mask = condition(samples)
+            accept_mask = keras.ops.convert_to_tensor(accept_mask, dtype="bool")
+
+            if not keras.ops.any(accept_mask):
+                continue
+
+            (accept_indices,) = keras.ops.nonzero(accept_mask)
+
+            samples = {key: keras.ops.take(value, accept_indices, axis=axis) for key, value in samples.items()}
+
+            if not result:
+                result = samples
+            else:
+                result = concatenate_dicts([result, samples], axis=axis)
+
+        if numpy:
+            result = {key: keras.ops.convert_to_numpy(value) for key, value in result.items()}
+
+        return result
