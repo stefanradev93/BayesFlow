00a372580f98f18415e8555f4f34989b9006cdbc
larskue <lars@kuehmichel.de>
2024-07-09 19:46:21 +0200

    add extra sample size to simulator rejection_sample

--- bayesflow/simulators/simulator.py
+++ bayesflow/simulators/simulator.py
@@ -9,12 +9,27 @@ class Simulator:
         raise NotImplementedError
 
     def rejection_sample(
-        self, batch_shape: Shape, condition: callable, *, axis: int = 0, numpy: bool = False, **kwargs
+        self,
+        batch_shape: Shape,
+        condition: callable,
+        *,
+        axis: int = 0,
+        numpy: bool = False,
+        extra_sample_size: int = None,
+        **kwargs,
     ) -> dict[str, Tensor]:
+        if extra_sample_size is None:
+            sample_shape = batch_shape
+        else:
+            sample_shape = list(batch_shape)
+            sample_shape[axis] = extra_sample_size
+
+            sample_shape = tuple(sample_shape)
+
         result = {}
 
         while not result or keras.ops.shape(next(iter(result.values())))[axis] < batch_shape[axis]:
-            samples = self.sample(batch_shape, **kwargs)
+            samples = self.sample(sample_shape, **kwargs)
             accept_mask = condition(samples)
             accept_mask = keras.ops.convert_to_tensor(accept_mask, dtype="bool")
 
