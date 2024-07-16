ab635f7fec6bce7f6c9b2e67ba5484b39ec73ab8
lars <lars@kuehmichel.de>
2024-07-16 16:44:44 +0200

    allow and change default base distribution to None

--- bayesflow/networks/summary_network.py
+++ bayesflow/networks/summary_network.py
@@ -6,9 +6,8 @@ from bayesflow.utils import find_distribution, keras_kwargs
 
 
 class SummaryNetwork(keras.Layer):
-    def __init__(self, base_distribution: str = "normal", **kwargs):
+    def __init__(self, base_distribution: str = None, **kwargs):
         super().__init__(**keras_kwargs(kwargs))
-
         self.base_distribution = find_distribution(base_distribution)
 
     def build(self, input_shape):
