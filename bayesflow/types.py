a14cfff6ee9be2fdc3b59f7b9c57960db7faf4a8
lars <lars@kuehmichel.de>
2024-07-17 10:52:19 +0200

    simplify Tensor type definition to allow better type inference for semi-dynamic type checkers

--- bayesflow/types.py
+++ bayesflow/types.py
@@ -1,32 +1,24 @@
-from typing import TYPE_CHECKING
+import keras
 
 Shape = tuple[int, ...]
 
 
-if TYPE_CHECKING:
-    import keras
+match keras.backend.backend():
+    case "numpy":
+        import numpy as np
 
-    match keras.backend.backend():
-        case "numpy":
-            import numpy as np
+        Tensor = np.ndarray
+    case "jax":
+        import jax
 
-            Tensor = np.ndarray
-        case "jax":
-            import jax
+        Tensor = jax.Array
+    case "tensorflow":
+        import tensorflow as tf
 
-            Tensor = jax.Array
-        case "tensorflow":
-            import tensorflow as tf
+        Tensor = tf.Tensor
+    case "torch":
+        import torch
 
-            Tensor = tf.Tensor
-        case "torch":
-            import torch
-
-            Tensor = torch.Tensor
-        case other:
-            raise NotImplementedError
-
-else:
-    import keras
-
-    Tensor = type(keras.ops.zeros(()))
+        Tensor = torch.Tensor
+    case other:
+        raise NotImplementedError
