f490ff520ea712da0fd84ce9994f796cb8bda1de
lars <lars@kuehmichel.de>
2024-07-23 17:35:27 +0200

    modularize example

--- examples/2024-07-19 Two Moons.py
+++ examples/2024-07-19 Two Moons.py
@@ -1,27 +1,36 @@
-import bayesflow as bf
-import keras
+def main():
+    import os
 
-epochs = 100
-steps_per_epoch = 100
-batch_size = 512
+    os.environ["KERAS_BACKEND"] = "torch"
 
-approximator = bf.ContinuousApproximator(
-    inference_network=bf.networks.FlowMatching(),
-)
+    import bayesflow as bf
+    import keras
 
-learning_rate = keras.optimizers.schedules.CosineDecay(1e-3, epochs * steps_per_epoch, 1e-6)
-optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    epochs = 100
+    steps_per_epoch = 100
+    batch_size = 512
 
-approximator.compile(optimizer=optimizer)
+    approximator = bf.ContinuousApproximator(
+        inference_network=bf.networks.FlowMatching(),
+    )
 
-approximator.fit(
-    simulator=bf.simulators.TwoMoonsSimulator(),
-    inference_variables=["theta"],
-    inference_conditions=["x"],
-    epochs=epochs,
-    steps_per_epoch=steps_per_epoch,
-    batch_size=batch_size,
-    # we don't need workers or multiprocessing because our dataset is small
-    workers=1,
-    use_multiprocessing=False,
-)
+    learning_rate = keras.optimizers.schedules.CosineDecay(1e-3, epochs * steps_per_epoch, 1e-6)
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+
+    approximator.compile(optimizer=optimizer)
+
+    approximator.fit(
+        simulator=bf.simulators.TwoMoonsSimulator(),
+        inference_variables=["theta"],
+        inference_conditions=["x"],
+        epochs=epochs,
+        steps_per_epoch=steps_per_epoch,
+        batch_size=batch_size,
+        # we don't need workers or multiprocessing because our dataset is small
+        workers=1,
+        use_multiprocessing=False,
+    )
+
+
+if __name__ == "__main__":
+    main()
