ad11d4b5b05e596922af93addaec40449a49c8bf
Chase-Grajeda <chase.grajeda@gmail.com>
2024-08-06 14:42:38 -0400

    Completed model
    Finished model in cif.py.
    Added conditional_gaussian.py for ConditionalGaussian helper class.
    Removed cif.ipynb example.
    Added moons_cif.ipynb example.

--- bayesflow/networks/cif/cif.py
+++ bayesflow/networks/cif/cif.py
@@ -1,75 +1,86 @@
 import keras
+from keras.saving import register_keras_serializable
 from ..inference_network import InferenceNetwork
 from ..coupling_flow import CouplingFlow
+from .conditional_gaussian import ConditionalGaussian
 
 
+@register_keras_serializable(package="bayesflow.networks")
 class CIF(InferenceNetwork):
-    def __init__(self, **kwargs):
+    """Implements a continuously indexed flow (CIF) with a `CouplingFlow` bijection and
+    `ConditionalGaussian` distributions p and q. Improves on eliminating leaky
+    sampling found topologically in normalizing flows. Bulit in reference to [1].
+    
+    [1] R. Cornish, A. Caterini, G. Deligiannidis, & A. Doucet (2021).
+    Relaxing Bijectivity Constraints with Continuously Indexed Normalising Flows.
+    arXiv:1909.13833.
+    """
+    
+    def __init__(self, pq_depth=4, pq_width=128, pq_activation="tanh", **kwargs):
+        """Creates an instance of a `CIF` with configurable `ConditionalGaussian` distributions
+        p and q, each containing MLP networks
+
+        Parameters:
+        -----------
+        pq_depth: int, optional, default: 4
+            The number of MLP hidden layers (minimum: 1)
+        pq_width: int, optional, default: 128
+            The dimensionality of the MLP hidden layers
+        pq_activation: str, optional, default: 'tanh'
+            The MLP activation function
+        """
+        
         super().__init__(base_distribution="normal", **kwargs)
-        # Member variables wrt to nux implementation
-        self.feature_net = CouplingFlow() 	 # no conditions
-        self.flow = CouplingFlow() 			 # bijective transformer
-        self.u_dist = self.base_distribution # Gaussian prior
-        self.v_dist = CouplingFlow()		 # conditioned flow / parameterized gaussian
+        self.bijection = CouplingFlow()
+        self.p_dist = ConditionalGaussian(depth=pq_depth, width=pq_width, activation=pq_activation)
+        self.q_dist = ConditionalGaussian(depth=pq_depth, width=pq_width, activation=pq_activation)
         
     
-    def build(self, xz_shape, conditions_shape):
-        super().build(xz_shape)            
-        self.feature_net.build(xz_shape)
-        self.flow.build(xz_shape, xz_shape)
-        self.v_dist.build(xz_shape, xz_shape)
+    def build(self, xz_shape, conditions_shape=None):
+        super().build(xz_shape)        
+        self.bijection.build(xz_shape, conditions_shape=conditions_shape)
+        self.p_dist.build(xz_shape)
+        self.q_dist.build(xz_shape)
         
     
-    def call(self, xz, conditions, inverse=False, **kwargs):
+    def call(self, xz, conditions=None, inverse=False, **kwargs):
         if inverse:
-            return self._inverse(xz, conditions, **kwargs)
-        return self._forward(xz, conditions, **kwargs)
+            return self._inverse(xz, conditions=conditions, **kwargs)
+        return self._forward(xz, conditions=conditions, **kwargs)
     
     
-    def _forward(self, x, conditions, density=False, **kwargs):
-        # NOTE: conditions should be used...
+    def _forward(self, x, conditions=None, density=False, **kwargs):
+        # Sample u ~ q_u
+        u, log_qu = self.q_dist.sample(x, log_prob=True)
+                
+        # Bijection and log jacobian x -> z
+        z, log_jac = self.bijection(x, conditions=conditions, density=True)
+        if log_jac.ndim > 1:
+            log_jac = keras.ops.sum(log_jac, axis=1)
         
-        # Sample u ~ q(u|phi_x)
-        phi_x = self.feature_net(x, conditions=None)
-        u, log_qu = self.v_dist(keras.ops.zeros_like(x), conditions=phi_x, inverse=True, density=True)
+        # Log prob over p on u with conditions z
+        log_pu = self.p_dist.log_prob(u, z)
         
-        # Compute z = f(x; phi_u) and p(x|u)
-        phi_u = self.feature_net(u, conditions=None)
-        z, log_px = self.flow(x, conditions=phi_u, inverse=False, density=True)
+        # Prior log prob
+        log_prior = self.base_distribution.log_prob(z)
+        if log_prior.ndim > 1:
+            log_prior = keras.ops.sum(log_prior, axis=1)
         
-        # Compute p(u)
-        log_pu = self.base_distribution.log_prob(u)
+        # ELBO loss
+        elbo = log_jac + log_pu + log_prior - log_qu
         
-        # Log likelihood?
-        llc = log_px + log_pu - log_qu
-        
-        # NOTE - this can be moved up when I'm done tinkering
         if density:
-            return z, llc
+            return z, elbo
         return z
     
     
-    def _inverse(self, z, conditions, density=False, **kwargs):
-        # NOTE: conditions should be used...
-        
-        # Sample u ~ p(u)
-        u = self.base_distribution.sample(keras.ops.shape(z)[:-1])
-        log_pu = self.base_distribution.log_prob(keras.ops.zeros_like(z))
-        
-        # Compute inverse of f(z; u)
-        phi_u = self.feature_net(u)
-        x, log_px = self.flow(z, conditions=phi_u, inverse=True, density=True)
-        
-        # Predict q(u|x)
-        phi_x = self.feature_net(x)
-        _, log_qu = self.v_dist(u, conditions=phi_x, inverse=False, density=True)
-        
-        # Log likelihood?
-        llc = log_px + log_pu - log_qu
-        
-        # NOTE: this can be moved up when I'm done tinkering
+    def _inverse(self, z, conditions=None, density=False, **kwargs):
+        # Inverse bijection z -> x
+        u = self.p_dist.sample(z)
+        x = self.bijection(z, conditions=conditions, inverse=True)
         if density:
-            return x, llc
+            log_pu = self.p_dist.log_prob(u, x)
+            return x, log_pu
         return x
     
     
@@ -77,10 +88,6 @@ class CIF(InferenceNetwork):
         base_metrics = super().compute_metrics(data, stage=stage)
         inference_variables = data["inference_variables"]
         inference_conditions = data.get("inference_conditions")
-        
-        z, log_density = self(inference_variables, conditions=inference_conditions, inverse=False, density=True)
-        # Should loss be reduced this way..?
-        loss = -keras.ops.mean(log_density)
+        _, elbo = self(inference_variables, conditions=inference_conditions, inverse=False, density=True)
+        loss = -keras.ops.mean(elbo)
         return base_metrics | {"loss": loss}
-        
-        
