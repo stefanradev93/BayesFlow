362ce5b521bce4fb02a2f2fe6c9a891ea72f360e
larskue <lars@kuehmichel.de>
2024-08-13 13:06:14 +0200

    fix size_of util for numpy arrays with non-numpy backend

--- bayesflow/utils/tensor_utils.py
+++ bayesflow/utils/tensor_utils.py
@@ -49,7 +49,7 @@ def size_of(x) -> int:
     :param x: A nested structure of tensors.
     :return: The total memory footprint of x, ignoring view semantics, in bytes.
     """
-    if keras.ops.is_tensor(x):
+    if keras.ops.is_tensor(x) or isinstance(x, np.ndarray):
         return int(keras.ops.size(x)) * np.dtype(keras.ops.dtype(x)).itemsize
 
     # flatten nested structure
