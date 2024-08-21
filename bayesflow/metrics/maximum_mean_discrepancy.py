be27554b91530367ed911ef47cbd43cd3380dc85
larskue <lars@kuehmichel.de>
2024-08-21 17:44:02 +0200

    fix mmd for batch sizes n != m

--- bayesflow/metrics/maximum_mean_discrepancy.py
+++ bayesflow/metrics/maximum_mean_discrepancy.py
@@ -5,8 +5,4 @@ from .functional import maximum_mean_discrepancy
 
 class MaximumMeanDiscrepancy(keras.metrics.MeanMetricWrapper):
     def __init__(self, name="maximum_mean_discrepancy", dtype=None, **kwargs):
-        def fn(y_true, y_pred):
-            mmd = maximum_mean_discrepancy(y_true, y_pred, **kwargs)
-            return keras.ops.mean(mmd, axis=1)
-
-        super().__init__(fn, name=name, dtype=dtype)
+        super().__init__(maximum_mean_discrepancy, name=name, dtype=dtype)
