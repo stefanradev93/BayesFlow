7eb20106b64fde1d6ca0917d52d30e9d71edf206
larskue <lars@kuehmichel.de>
2024-09-12 17:45:36 +0200

    allow using transforms in concatenate keys data adapter

--- bayesflow/data_adapters/concatenate_keys_data_adapter.py
+++ bayesflow/data_adapters/concatenate_keys_data_adapter.py
@@ -1,11 +1,14 @@
 from collections.abc import Mapping, Sequence
 from keras.saving import (
+    deserialize_keras_object as deserialize,
     register_keras_serializable as serializable,
+    serialize_keras_object as serialize,
 )
 import numpy as np
 
 from .composite_data_adapter import CompositeDataAdapter
 from .data_adapter import DataAdapter
+from .transforms import Transform
 
 TRaw = Mapping[str, np.ndarray]
 TProcessed = np.ndarray | None
@@ -68,14 +71,35 @@ class _ConcatenateKeysDataAdapter(DataAdapter[TRaw, TProcessed]):
 class ConcatenateKeysDataAdapter(CompositeDataAdapter):
     """Concatenates data from multiple keys into multiple tensors."""
 
-    def __init__(self, **keys: Sequence[str]):
+    def __init__(self, *, transforms: Sequence[Transform] = None, **keys: Sequence[str]):
+        self.transforms = transforms or []
         self.keys = keys
         configurators = {key: _ConcatenateKeysDataAdapter(value) for key, value in keys.items()}
         super().__init__(configurators)
 
+    def configure(self, raw_data):
+        data = raw_data
+
+        for transform in self.transforms:
+            data = transform(data, inverse=True)
+
+        data = super().configure(data)
+
+        return data
+
+    def deconfigure(self, processed_data):
+        data = processed_data
+
+        data = super().deconfigure(data)
+
+        for transform in reversed(self.transforms):
+            data = transform(data, inverse=False)
+
+        return data
+
     @classmethod
     def from_config(cls, config: Mapping[str, any], custom_objects=None) -> "ConcatenateKeysDataAdapter":
-        return cls(**config["keys"])
+        return cls(**config["keys"], transforms=deserialize(config.get("transforms")))
 
     def get_config(self) -> dict[str, any]:
-        return {"keys": self.keys}
+        return {"keys": self.keys, "transforms": serialize(self.transforms)}
