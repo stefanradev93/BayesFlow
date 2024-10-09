Implement requested changes - fix normalization constant for multi-dim - allow learnable parameters - allow numpy array arguments - use += with normalization constant - rename mean, std -> loc, scale for StudentT - rename degrees_of_freedom to df - also apply these improvements to DiagonalNormal - reorder imports

bayesflow/distributions/diagonal_normal.py
@@ -1,7 +1,8 @@
-import math
-
 import keras
 from keras.saving import register_keras_serializable as serializable
+
+import math
+import numpy as np
 
 from bayesflow.types import Shape, Tensor
 from .distribution import Distribution
@@ -13,8 +14,9 @@ class DiagonalNormal(Distribution):
 
     def __init__(
         self,
-        mean: float | Tensor = 0.0,
-        std: float | Tensor = 1.0,
+        mean: int | float | np.ndarray | Tensor = 0.0,
+        std: int | float | np.ndarray | Tensor = 1.0,
+        use_learnable_parameters: bool = False,
         seed_generator: keras.random.SeedGenerator = None,
         **kwargs,
     ):
@@ -22,23 +24,57 @@ class DiagonalNormal(Distribution):
         self.mean = mean
         self.std = std
         self.var = std**2
+
         self.dim = None
         self.log_normalization_constant = None
-        self.seed_generator = seed_generator or keras.random.SeedGenerator()
+
+        self.use_learnable_parameters = use_learnable_parameters
+
+        if seed_generator is None:
+            seed_generator = keras.random.SeedGenerator()
+
+        self.seed_generator = seed_generator
 
     def build(self, input_shape: Shape) -> None:
         self.dim = int(input_shape[-1])
-        self.log_normalization_constant = 0.5 * self.dim * (math.log(2.0 * math.pi) + math.log(self.var))
+
+        # convert to tensor and broadcast if necessary
+        self.mean = keras.ops.broadcast_to(self.mean, (self.dim,))
+        self.mean = keras.ops.cast(self.mean, "float32")
+
+        self.std = keras.ops.broadcast_to(self.std, (self.dim,))
+        self.std = keras.ops.cast(self.std, "float32")
+
+        # we have to do this again because the type of self.std may have changed
+        self.var = self.std**2
+
+        self.log_normalization_constant = (
+            -0.5 * self.dim * (math.log(2.0 * math.pi) - keras.ops.sum(keras.ops.log(self.var)))
+        )
+
+        if self.use_learnable_parameters:
+            mean = self.mean
+            self.mean = self.add_weight(
+                shape=keras.ops.shape(mean),
+                initializer="zeros",
+                dtype="float32",
+            )
+
+            std = self.std
+            self.std = self.add_weight(
+                shape=keras.ops.shape(std),
+                initializer="ones",
+                dtype="float32",
+            )
+            self.std.assign(std)
 
     def log_prob(self, samples: Tensor, *, normalize: bool = True) -> Tensor:
-        result = -0.5 * keras.ops.sum((samples - self.mean) ** 2, axis=-1) / self.var
+        result = -0.5 * keras.ops.sum((samples - self.mean) ** 2 / self.var, axis=-1)
 
         if normalize:
-            result -= self.log_normalization_constant
+            result += self.log_normalization_constant
 
         return result
 
     def sample(self, batch_shape: Shape) -> Tensor:
-        return keras.random.normal(
-            shape=batch_shape + (self.dim,), mean=self.mean, stddev=self.std, seed=self.seed_generator
-        )
+        return self.mean + self.std * keras.random.normal(shape=batch_shape + (self.dim,), seed=self.seed_generator)

bayesflow/distributions/diagonal_student_t.py
@@ -1,9 +1,10 @@
-import math
-
-from scipy.stats import t as scipy_student_t
-
 import keras
 from keras.saving import register_keras_serializable as serializable
+
+import math
+import numpy as np
+
+from scipy.stats import t as scipy_student_t
 
 from bayesflow.types import Shape, Tensor
 from .distribution import Distribution
@@ -15,47 +16,71 @@ class DiagonalStudentT(Distribution):
 
     def __init__(
         self,
-        degrees_of_freedom: int = 50,
-        mean: float | Tensor = 0.0,
-        std: float | Tensor = 1.0,
-        seed_generator: keras.random.SeedGenerator = None,
+        df: int | float = 50,
+        loc: int | float | np.ndarray | Tensor = 0.0,
+        scale: int | float | np.ndarray | Tensor = 1.0,
+        use_learnable_parameters: bool = False,
         **kwargs,
     ):
         super().__init__(**kwargs)
 
-        self.degrees_of_freedom = degrees_of_freedom
-        self.mean = mean
-        self.std = std
-        self.seed_generator = seed_generator or keras.random.SeedGenerator()
+        self.df = df
+        self.loc = loc
+        self.scale = scale
 
         self.dim = None
         self.log_normalization_constant = None
 
+        self.use_learnable_parameters = use_learnable_parameters
+
     def build(self, input_shape: Shape) -> None:
         self.dim = int(input_shape[-1])
+
+        # convert to tensor and broadcast if necessary
+        self.loc = keras.ops.broadcast_to(self.loc, (self.dim,))
+        self.loc = keras.ops.cast(self.loc, "float32")
+
+        self.scale = keras.ops.broadcast_to(self.scale, (self.dim,))
+        self.scale = keras.ops.cast(self.scale, "float32")
+
         self.log_normalization_constant = (
-            0.5 * self.dim * math.log(self.degrees_of_freedom)
-            + 0.5 * self.dim * math.log(math.pi)
-            + math.lgamma(0.5 * self.degrees_of_freedom)
-            - math.lgamma(0.5 * (self.degrees_of_freedom + self.dim))
-            + 0.5 * keras.ops.sum(keras.ops.log(self.std))
+            -0.5 * self.dim * math.log(self.df)
+            - 0.5 * self.dim * math.log(math.pi)
+            - math.lgamma(0.5 * self.df)
+            + math.lgamma(0.5 * (self.df + self.dim))
+            - 0.5 * keras.ops.sum(keras.ops.log(self.scale))
         )
+
+        if self.use_learnable_parameters:
+            loc = self.loc
+            self.loc = self.add_weight(
+                shape=keras.ops.shape(loc),
+                initializer="zeros",
+                dtype="float32",
+            )
+            self.loc.assign(loc)
+
+            scale = self.scale
+            self.scale = self.add_weight(
+                shape=keras.ops.shape(scale),
+                initializer="ones",
+                dtype="float32",
+            )
+            self.scale.assign(scale)
 
     def log_prob(self, samples: Tensor, *, normalize: bool = True) -> Tensor:
-        mahalanobis_term = keras.ops.sum((samples - self.mean) ** 2 / self.std**2, axis=-1)
-        result = (
-            -0.5 * (self.degrees_of_freedom + self.dim) * keras.ops.log1p(mahalanobis_term / self.degrees_of_freedom)
-        )
+        mahalanobis_term = keras.ops.sum((samples - self.loc) ** 2 / self.scale**2, axis=-1)
+        result = -0.5 * (self.df + self.dim) * keras.ops.log1p(mahalanobis_term / self.df)
 
         if normalize:
-            result -= self.log_normalization_constant
+            result += self.log_normalization_constant
 
         return result
 
     def sample(self, batch_shape: Shape) -> Tensor:
         # TODO: use reparameterization trick instead of scipy
         # TODO: use the seed generator state
-        dist = scipy_student_t(df=self.degrees_of_freedom, loc=self.mean, scale=self.std)
+        dist = scipy_student_t(df=self.df, loc=self.loc, scale=self.scale)
         samples = dist.rvs(size=batch_shape + (self.dim,))
 
         return keras.ops.convert_to_tensor(samples)